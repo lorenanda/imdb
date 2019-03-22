29e62c418d42220de79729c5eef7cbda229802c6
Lorena <lorena.ciutacu@gmail.com>
2019-03-22 23:15:14 +0100

    add charts

--- imdb_top_2018.R
+++ imdb_top_2018.R
@@ -5,9 +5,9 @@ url = "https://www.imdb.com/search/title?year=2018"
 imdb = read_html(url)
 head(imdb)
 
+#scrape the movie rankings
 rank_data_html = html_nodes(imdb, '.lister-item-index unbold text-primary')
 class(rank_data_html)
-
 rank_data = html_text(rank_data_html)
 head(rank_data)
 class(rank_data)
@@ -43,7 +43,7 @@ head(genre_data)
 genre_data = gsub("\n", "", genre_data)
 head(genre_data)
 
-#remove the blanks between genres
+#remove the spaces between genres
 genre_data = gsub(" ", "", genre_data)
 head(genre_data)
 
@@ -53,6 +53,10 @@ head(genre_data)
 
 #create a dataframe with the scraped movie infos
 imdb_df = data.frame(Title=title_data, Genre=genre_data, Runtime=runtime_data)
+
+#descriptive stats
+median(runtime_data)
+mean(runtime_data)
 
 #plot the number of movies by genre
 #barplot(table(imdb_df$Genre))
@@ -65,13 +69,18 @@ ggplot(imdb_df, aes(x=genre_data)) +
 #plot the movies by runtime
 barplot(table(imdb_df$Runtime))
 hist(imdb_df$Runtime)
-ggplot(imdb_df, aes(x=runtime_data)) + geom_histogram(color="purple", fill="green", alpha=0.3) 
+ggplot(imdb_df, aes(x=runtime_data)) +
+  geom_histogram(color="purple", fill="green", alpha=0.3) +
+  ggtitle("Distribution of movie runtimes") +
+  xlab("Minutes") + ylab("Number of movies")
 
 #group movies by genre
 library(dplyr)
-genre_cat <- group_by(imdb_df, Genre)
-genre_runtime <- summarize(genre_cat, Minutes=median(Runtime))
+genre_cat = group_by(imdb_df, Genre)
+genre_runtime = summarize(genre_cat, Minutes=mean(Runtime))
 plot(genre_runtime)
 
-counts <- table(genre_runtime$Genre, genre_runtime$Minutes)
-ggplot(data=genre_runtime, aes(x=Genre, y=Minutes)) + geom_bar(stat = "identity") 
+counts = table(genre_runtime$Genre, genre_runtime$Minutes)
+ggplot(data=genre_runtime, aes(x=Genre, y=Minutes)) +
+  geom_bar(stat = "identity", color="purple", fill="green", alpha=0.3) +
+  ggtitle("Mean movie duration by genre") 
